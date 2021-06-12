151ba1dde7a743e698baf9cc8c807fed9efd5ae7
Anton Wiklund <ixuz07@gmail.com>
2021-06-12 15:21:47 +0200

    wip

--- Lance Master/Assets/Scripts/Weapon.cs
+++ Lance Master/Assets/Scripts/Weapon.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
     public float fireRate = 1f;
     
-    private bool shooting = false;
+    private bool trigger = false;
+    private float fireCooldown = 0f;
 
     private void Update() {
-        
+        fireCooldown = Mathf.Min(0, fireCooldown - Time.deltaTime);
+
+        if (IsTriggerDown() && !IsCooldown()) {
+            
+        }
     }
 
     public void Aim(Vector2 direction) {
@@ -16,7 +21,18 @@ public class Weapon : MonoBehaviour {
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
-    public void Shoot(bool shooting) {
-        this.shooting = shooting;
+    public void SetTrigger(bool trigger) {
+        this.trigger = trigger;
+    }
+
+    public bool IsTriggerDown() {
+        return trigger;
+    }
+    public bool IsTriggerUp() {
+        return !IsTriggerDown();
+    }
+
+    public bool IsCooldown() {
+        return fireCooldown > 0;
     }
 }
