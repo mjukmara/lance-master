b08d628a0ad43d1e5d37730c00ff8be1c87cc3e1
Yam0 <tage.akerstrom@gmail.com>
2021-06-12 13:56:20 +0200

    Small change

---
+++ Lance Master/Assets/Scripts/GroupManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroupManager : MonoBehaviour
+{
+	public Group[] groups;
+}
+
+public struct Group
+{
+	public GameObject spawntrigger;
+	public GameObject[] Enemies;
+	public GameObject nextDoor;
+}
