9dd69858e3ebd0710ad46b8d37a4e6437f8e8324
Yam0 <tage.akerstrom@gmail.com>
2021-06-12 22:10:46 +0200

    Player animation

--- Lance Master/Assets/Scripts/Player.cs
+++ Lance Master/Assets/Scripts/Player.cs
@@ -6,22 +6,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	public int health = 100;
-	public float moveAcceleration = 4f;
-	public float moveMaxSpeed = 4f;
-	public float dashCooldown = 1f;
-	public float dashMaxLength = 5f;
-	public float dashForce = 5f;
-	public float drag = 40f;
-	public float dashDamageDuration = 1f;
-	public float dashInvinsibleDuration = 1f;
+
 	private CharacterController cc;
 	private Transform tr;
+	private Transform spritesTransform;
+	private Animator animator;
 	public GameObject arrow;
 
 	private void OnEnable()
 	{
 		cc.OnDashEvent += OnDash;
-
 	}
 
 	private void OnDisable()
@@ -33,11 +27,23 @@ public class Player : MonoBehaviour
 	{
 		cc = GetComponent<CharacterController>();
 		tr = GetComponent<Transform>();
+		spritesTransform = gameObject.transform.Find("Sprites").GetComponent<Transform>();
+		animator = gameObject.transform.Find("Sprites").GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
-		cc.SetMoveInput(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")));
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		cc.SetMoveInput(input);
+		float animInput = Mathf.Abs(Mathf.Sqrt(Mathf.Pow(input.x, 2) + Mathf.Pow(input.y, 2)));
+		animator.SetFloat("Input", animInput);
+		if (Mathf.Abs(input.x) > 0.1f)
+		{
+			float direction = (input.x < 0) ? -1 : 1;
+			Vector3 spriteScale = spritesTransform.localScale;
+			spriteScale.x = direction;
+			spritesTransform.localScale = spriteScale;
+		}
 
 		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0) || Input.GetButtonDown("Fire1") || Input.GetButtonDown("Right Bumper"))
 		{
@@ -50,18 +56,20 @@ public class Player : MonoBehaviour
 		gameObject.transform.Find("DashTrail").gameObject.GetComponent<ParticleSystem>().Play();
 		float rz = Mathf.Atan2(cc.GetMoveDirection().y, cc.GetMoveDirection().x) * Mathf.Rad2Deg;
 		Instantiate(arrow, tr.position, Quaternion.AngleAxis(rz, Vector3.forward));
-
+		animator.SetBool("Dashing", true);
 		string[] soundNames = new string[] { "dash1", "dash2", "dash3", "dash4", "dash5" };
 		AudioManager.Instance.PlaySfx(soundNames[Random.Range(0, soundNames.Length)]);
 	}
 
-	public void Hit(int damage) {
+	public void Hit(int damage)
+	{
 		health = Mathf.Max(0, health - damage);
 
 		string[] soundNames = new string[] { "ow1", "ow2", "ow3", "ow4", "ow5", "ow6" };
 		AudioManager.Instance.PlaySfx(soundNames[Random.Range(0, soundNames.Length)]);
 
-		if (health == 0) {
+		if (health == 0)
+		{
 			Destroy(gameObject);
 		}
 	}
