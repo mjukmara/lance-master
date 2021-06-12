e83fcd653c8b346dc70bd8d41ff638d535ff4662
Yam0 <tage.akerstrom@gmail.com>
2021-06-12 15:09:18 +0200

    Added group manager

--- Lance Master/Assets/Scripts/GroupManager.cs
+++ Lance Master/Assets/Scripts/GroupManager.cs
@@ -4,12 +4,60 @@ using UnityEngine;
 
 public class GroupManager : MonoBehaviour
 {
-	public Group[] groups;
-}
+	public List<GameObject> enemies;
+	private List<GameObject> spawnedEnemies;
+	private bool triggered = false;
+	private bool spawned = false;
 
-public struct Group
-{
-	public GameObject spawntrigger;
-	public GameObject[] Enemies;
-	public GameObject nextDoor;
+	void Update()
+	{
+		if (triggered && !spawned)
+		{
+			Debug.Log("Spawning enemies!");
+			spawnedEnemies = new List<GameObject>();
+
+			//Spawn enemies at child "spawnPoint_<i>" gameObject positions
+			for (int i = 0; i < enemies.Count; i++)
+			{
+				if (i > gameObject.transform.Find("spawnPoints").childCount - 1) { continue; }
+				Vector3 spawnPoint = gameObject.transform.Find("spawnPoints").GetChild(i).position;
+				spawnedEnemies.Add((GameObject)Instantiate(enemies[i], spawnPoint, Quaternion.identity));
+			}
+
+			spawned = true;
+		}
+
+		if (spawned)
+		{
+			if (Finished())
+			{
+				Debug.Log("Open the door!");
+				GameObject door = gameObject.transform.Find("door").gameObject;
+				door.GetComponent<BoxCollider2D>().enabled = false;
+			}
+		}
+	}
+
+	bool Finished()
+	{
+		bool isFinished = true;
+
+		for (int i = 0; i < spawnedEnemies.Count; i++)
+		{
+			if (spawnedEnemies[i] != null)
+			{
+				isFinished = false;
+			}
+		}
+
+		return isFinished;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			triggered = true;
+		}
+	}
 }
