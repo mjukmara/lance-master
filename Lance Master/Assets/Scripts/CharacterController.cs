1c32995c61bbd92bff5e7ce50e4ee22f96516df1
Anton Wiklund <ixuz07@gmail.com>
2021-06-12 11:54:55 +0200

    decoupled

--- Lance Master/Assets/Scripts/CharacterController.cs
+++ Lance Master/Assets/Scripts/CharacterController.cs
@@ -6,21 +6,15 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float spass = 0.6f;
-    public float deadZone = 0.1f;
     public float drag = 40f;
     public float moveAcceleration = 4f;
-    private Vector2 perlinSeed;
     Vector2 moveDirection = Vector2.zero;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        perlinSeed = new Vector2(Random.Range(0, 10000), Random.Range(0, 10000));
     }
 
     private void Update() {
-        UpdateMoveDirection();
-
         Debug.DrawLine(transform.position, transform.position + new Vector3(moveDirection.x, moveDirection.y), Color.blue);
     }
 
@@ -35,22 +29,7 @@ public class CharacterController : MonoBehaviour
         rb.drag = drag;
     }
 
-    private void UpdateMoveDirection() {
-        float x = Mathf.PerlinNoise(perlinSeed.x + Time.time * spass, 0f);
-        float y = Mathf.PerlinNoise(0f, perlinSeed.x + Time.time * spass);
-
-        if (x > 0.5f + deadZone) x = 1f;
-        else if (x < 0.5f - deadZone) x = 0f;
-        else x = 0.5f;
-
-        if (y > 0.5f + deadZone) y = 1f;
-        else if (y < 0.5f - deadZone) y = 0f;
-        else y = 0.5f;
-
-        x -= 0.5f;
-        y -= 0.5f;
-        x *= 2f;
-        y *= 2f;
-        moveDirection = new Vector2(x, y);
+    public void SetMoveInput(Vector2 axis) {
+        moveDirection = axis;
     }
 }
--- Lance Master/Assets/Scripts/Enemy.cs
+++ Lance Master/Assets/Scripts/Enemy.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Enemy : MonoBehaviour
 {
+    public float spass = 0.6f;
+    public float deadZone = 0.1f;
     private CharacterController cc;
     private Transform target = null;
     private Vector2 targetDirection = Vector2.zero;
+    private Vector2 perlinSeed;
 
     private void Start() {
         cc = GetComponent<CharacterController>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
+        perlinSeed = new Vector2(Random.Range(0, 10000), Random.Range(0, 10000));
     }
 
     private void Update() {
+        UpdateMoveDirection();
         UpdateTargeting();
         Debug.DrawLine(transform.position, transform.position + new Vector3(targetDirection.x, targetDirection.y), Color.red);
     }
@@ -25,5 +30,25 @@ public class Enemy : MonoBehaviour
 
     private void UpdateTargeting() {
         targetDirection = (target.position - transform.position).normalized;
+    }
+
+    private void UpdateMoveDirection() {
+        float x = Mathf.PerlinNoise(perlinSeed.x + Time.time * spass, 0f);
+        float y = Mathf.PerlinNoise(0f, perlinSeed.x + Time.time * spass);
+
+        if (x > 0.5f + deadZone) x = 1f;
+        else if (x < 0.5f - deadZone) x = 0f;
+        else x = 0.5f;
+
+        if (y > 0.5f + deadZone) y = 1f;
+        else if (y < 0.5f - deadZone) y = 0f;
+        else y = 0.5f;
+
+        x -= 0.5f;
+        y -= 0.5f;
+        x *= 2f;
+        y *= 2f;
+
+        cc.SetMoveInput(new Vector2(x, y));
     }
 }
