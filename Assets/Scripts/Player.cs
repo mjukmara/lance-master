6f6f7aebc10af31376cb93370ab794e24fa23fc4
Anton Wiklund <ixuz07@gmail.com>
2021-06-12 09:15:59 +0200

    dashing damage & invincibility flags

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,17 @@ public class Player : MonoBehaviour
     public float dashMaxLength = 5f;
     public float dashForce = 20f;
     public float drag = 20f;
+    public float dashDamageDuration = 1f;
+    public float dashInvinsibleDuration = 1f;
     private Rigidbody2D rb;
     private Vector2 moveInputVector = Vector2.zero;
     private Vector2 dashInputVector = Vector2.zero;
     private Vector2 mouseInput = Vector2.zero;
     private bool dashInput = false;
-    private float dashCooldownRemaining = 0f;
+    private float dashDamageCooldown = 0f;
+    private float dashInvinsibleCooldown = 0f;
+    private bool attacking = false;
+    private bool invinsible = false;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -28,6 +33,14 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) {
             dashInput = true;
         }
+
+        if (dashDamageCooldown > 0) dashDamageCooldown -= Time.deltaTime;
+        else dashDamageCooldown = 0;
+        attacking = (dashDamageCooldown > 0);
+
+        if (dashInvinsibleCooldown > 0) dashInvinsibleCooldown -= Time.deltaTime;
+        else dashInvinsibleCooldown = 0;
+        invinsible = (dashInvinsibleCooldown > 0);
     }
 
     private void FixedUpdate() {
@@ -47,6 +60,8 @@ public class Player : MonoBehaviour
             Vector2 dashDirection = GetDashDirection();
             Vector2 force = dashDirection * dashForce * 1000f;
             rb.AddForce(force);
+            dashDamageCooldown = dashDamageDuration;
+            dashInvinsibleCooldown = dashInvinsibleDuration;
         }
         dashInput = false;
     }
