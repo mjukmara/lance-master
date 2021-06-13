92dae5e2faca2fb84b5a04c3ab7e61beb3584ac3
Yam0 <tage.akerstrom@gmail.com>
2021-06-13 17:34:07 +0200

    Victory pickup

---
+++ Lance Master/Assets/VictoryPickup.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryPickup : MonoBehaviour
+{
+	public GameObject pickupEffect;
+	public float delay = 1.0f;
+	bool pickup = false;
+
+	private void Update()
+	{
+		if (pickup && delay > 0)
+		{
+			delay -= Time.deltaTime;
+			//Game.Instance;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			Instantiate(pickupEffect, gameObject.transform.position, Quaternion.identity);
+
+		}
+	}
+}
